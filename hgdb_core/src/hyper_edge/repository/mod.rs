d16979603325b532f1d8f0b39d8b935a6c7f4b36
esmat-hejazi <esmat.hejazi@gmail.com>
2025-03-19 15:13:59 +0100

    Add Repository trait

--- hgdb_core/src/hyper_edge/repository/mod.rs
+++ hgdb_core/src/hyper_edge/repository/mod.rs
@@ -1,3 +1,14 @@
 pub mod simple_h_edge_repository;
 pub mod light_h_edge_repository;
 pub mod h_edge_repository;
+
+pub trait Repository<T> {
+    fn create(&self, entity:&T) -> Result<(), Box<dyn std::error::Error>>;
+    fn get_by_key(&self, key:&str) -> Option<T>;
+    fn get_all(&self) -> Vec<T>;
+    fn update(&self, key:&str, entity:&T) -> Result<(), Box<dyn std::error::Error>>;
+    fn update_and_get(&self, key:String, entity:T) -> Result<T, Box<dyn std::error::Error>>;
+    fn delete_by_key(&self, key:&str) -> Result<(), Box<dyn std::error::Error>>;
+    fn delete_all(&self, keys: Vec<String>) -> Result<(), Box<dyn std::error::Error>>;
+
+}
