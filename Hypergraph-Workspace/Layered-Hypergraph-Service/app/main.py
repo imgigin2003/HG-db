1aa2887549ed57c551829185061658a2c3fd8016
mahsaabdolahnezhad <141028214+mahsaabdolahnezhad@users.noreply.github.com>
2026-02-03 14:27:10 +0330

    removed python files related to data and rendering

--- Hypergraph-Workspace/Layered-Hypergraph-Service/app/main.py
+++ Hypergraph-Workspace/Layered-Hypergraph-Service/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
-from app.api.layered import router as layered_router
+
 
 app = FastAPI()
 
@@ -12,7 +12,6 @@ app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
 
 templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
-app.include_router(layered_router)
 
 @app.get("/viewer")
 def viewer(request: Request):
